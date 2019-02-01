78c955e20cbd10a3fd0f47bbe6e1b0054aad4e95
Catalin Sandru <catalin.sandru88@gmail.com>
2019-02-01 20:44:19 +0000

    added a bit of style to match inputs and description

--- src/components/create_acc/create_acc.js
+++ src/components/create_acc/create_acc.js
@@ -36,39 +36,41 @@ class AddAcc extends Component {
   render() {
     return(
       <div className="input_wrapper">
-        <h4>Enter Username
-          <input
-            ref={name => this.nameInput = name}/>
-            </h4>
-        <h4>Enter Password
-          <input
-            ref={pass => this.passwordInput = pass}/>
-         </h4> 
-        <h4>Enter E-Mail
-          <input
-            ref={email => this.emailInput = email}/>
-         </h4> 
-        <h4>Enter First Name
-          <input
-            ref={first_name => this.first_nameInput = first_name}/>
-         </h4> 
-        <h4>Enter Last Name
-          <input
-            ref={last_name => this.last_nameInput = last_name}/>
-         </h4> 
-        <h4>Select Gender
-          <input
-            ref={gender => this.genderInput = gender}/>
-         </h4> 
-        <h4>Upload profile pic
-          <input
-            ref={pic => this.picInput = pic}/>
-         </h4> 
-        <h4>Enter a short description
-          <input
-            ref={about => this.aboutInput = about}/>
-         </h4> 
-        <button onClick = {this.pushAcc} type="submit"> SIGN UP </button>
+        <div>
+          <h4>Enter Username</h4>
+          <input ref={name => this.nameInput = name}/>
+        </div>
+        <div>
+          <h4>Enter Password</h4> 
+          <input ref={pass => this.passwordInput = pass}/>
+        </div>
+        <div>
+          <h4>Enter E-Mail</h4> 
+          <input ref={email => this.emailInput = email}/>
+        </div>
+        <div>
+          <h4>Enter First Name</h4> 
+          <input ref={first_name => this.first_nameInput = first_name}/>
+        </div>
+        <div>
+          <h4>Enter Last Name</h4> 
+          <input ref={last_name => this.last_nameInput = last_name}/>
+        </div>
+        <div>
+          <h4>Select Gender</h4> 
+          <input ref={gender => this.genderInput = gender}/>
+        </div>
+        <div>
+          <h4>Upload profile pic</h4> 
+          <input ref={pic => this.picInput = pic}/>
+        </div>
+        <div>
+          <h4>Enter a short description</h4> 
+          <input ref={about => this.aboutInput = about}/>
+        </div>
+        <div>
+          <button onClick = {this.pushAcc} type="submit"> SIGN UP </button>
+        </div>
       </div>
     )
   }
