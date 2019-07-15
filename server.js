2c2aedcfde18310f0bdf87ff1ea5a7fadfe6e7c9
freebiker2000 <catalin.sandru88@gmail.com>
2019-07-15 20:32:53 +0100

    created get post and delete routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 // Bodyparser middleware
@@ -15,6 +17,9 @@ mongoose
   })
   .then(() => console.log('mongoDB connected'))
   .catch(err => console.log(err));
+
+// use routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
