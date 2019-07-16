3ab7d570838f0414a94bab4a8300c51c2d8e106b
freebiker2000 <catalin.sandru88@gmail.com>
2019-07-16 16:04:30 +0100

    added navbar from reactstrap

---
+++ client/src/component/App.navbar.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from 'reactstrap';
+
+const AppNavbar = () => {
+
+  const [ state, toogle ] = useState({
+    isOpen: false
+  })
+  console.log(state)
+
+  return(
+    <div>
+      <Navbar color="dark" dark expand="sm" className="mb-5">
+        <Container>
+          <NavbarBrand href="/">Shopping List</NavbarBrand>
+          <NavbarToggler onClick={() => toogle(state ? false : true)} />
+          <Collapse isOpen={state} navbar >
+            <Nav className="ml-auto" navbar >
+              <NavItem>
+                <NavLink href="https://github.com/freebiker2000">
+                  Github
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default AppNavbar;
