d9af81a915c8a0edca4d6a1d82ba4eb29747bff4
freebiker2000 <catalin.sandru88@gmail.com>
2019-07-28 14:12:44 +0100

    setup auth and error reducers

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import itemReducer from './reducer/itemReducer'
+import authReducer from './reducer/authReducer'
+import errorReducer from './reducer/errorReducer'
 
 export default combineReducers({
-  itemReducer
+  itemReducer,
+  authReducer,
+  errorReducer
 })
