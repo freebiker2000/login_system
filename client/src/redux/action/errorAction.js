8561bb724351d68b66cf7dcc7b3a41f258d298c6
freebiker2000 <catalin.sandru88@gmail.com>
2019-12-20 12:31:50 +0000

    made items to be deleted and added with token auth

--- client/src/redux/action/errorAction.js
+++ client/src/redux/action/errorAction.js
@@ -4,7 +4,7 @@ import { GET_ERRORS, CLEAR_ERRORS } from './types';
 export const returnErrors = (message, status, id = null) => {
   return {
     type: GET_ERRORS,
-    payload: { message, status, id}
+    payload: { message, status, id }
   }
 }
 
